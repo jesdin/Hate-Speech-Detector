e8be5a5ac554a6d840c54c253da2bbc9c8e4cec2
Jesdin Raphael <jesdinraphael@gmail.com>
2021-04-29 01:22:11 +0530

    add script file
    on click down arrow scrolls to detector

---
+++ static/scripts/detection.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#indicator").click(function(){
+    // window.scrollTo(0,document.body.scrollHeight/3);
+    document.getElementById("bd").scrollIntoView(2);
+  });
+});
